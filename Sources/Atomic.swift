bb1b7f1ee6913f32b2e30bfd11e55d8e56f9757e
Egzon Pllana <27929436+egzonpllana@users.noreply.github.com>
2023-04-25 00:04:06 +0200

    Update Atomic.swift
    The previous one was not really updating the value in the thread-safe way. Thanks to A.I, we investigated the behavior and fixed it.

--- Sources/Atomic.swift
+++ Sources/Atomic.swift
@@ -1,5 +1,5 @@
 @propertyWrapper
-struct Atomic<Value> {
+class Atomic<Value> {
     private var value: Value
     private let lock = NSLock()
 
@@ -13,19 +13,54 @@ struct Atomic<Value> {
             defer { lock.unlock() }
             return value
         }
-        set {
-            lock.lock()
-            defer { lock.unlock() }
-            value = newValue
-        }
+    }
+
+    func mutate(_ mutation: (inout Value) -> Void) {
+        lock.lock()
+        defer { lock.unlock() }
+        mutation(&value)
     }
 }
 
 // Usage:
 class Counter {
-    @Atomic private(set) var count: Int = 0
+    @Atomic var value: Int = 0
 
     func increment() {
-        count += 1
+        _value.mutate { $0 += 1}
     }
 }
+
+// Test that Atomic is really working with thread safe
+func testAtomicChange() {
+        // Create an instance of the Test class
+
+        // Define the number of iterations and concurrent threads
+        let iterations = 10_000
+        let numberOfThreads = 4
+
+        // Create a concurrent dispatch queue and a dispatch group
+        let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
+        let dispatchGroup = DispatchGroup()
+
+        // Perform concurrent reads and writes on the shared resource
+        for _ in 0..<iterations {
+            for _ in 0..<numberOfThreads {
+                dispatchGroup.enter()
+                concurrentQueue.async {
+                    self.counter.increment()
+                    dispatchGroup.leave()
+                }
+            }
+        }
+
+        // Wait for all tasks to finish
+        dispatchGroup.wait()
+
+        // Validate the result
+        let expectedValue = iterations * numberOfThreads
+        assert(counter.value == expectedValue, "Expected value: \(expectedValue), Actual value: \(counter.value)")
+}
+
+        print("Test passed. Expected value: \(expectedValue), Actual value: \(counter.value)")
+    }
