1ada478e8c50179c55ec24646a5f9cc3ecda2d22
Egzon Pllana <27929436+egzonpllana@users.noreply.github.com>
2023-04-25 00:06:24 +0200

    Update Atomic.swift

--- Sources/Atomic.swift
+++ Sources/Atomic.swift
@@ -2,11 +2,11 @@
 class Atomic<Value> {
     private var value: Value
     private let lock = NSLock()
-
+    
     init(wrappedValue: Value) {
         self.value = wrappedValue
     }
-
+    
     var wrappedValue: Value {
         get {
             lock.lock()
@@ -14,7 +14,7 @@ class Atomic<Value> {
             return value
         }
     }
-
+    
     func mutate(_ mutation: (inout Value) -> Void) {
         lock.lock()
         defer { lock.unlock() }
@@ -25,7 +25,7 @@ class Atomic<Value> {
 // Usage:
 class Counter {
     @Atomic var value: Int = 0
-
+    
     func increment() {
         _value.mutate { $0 += 1}
     }
@@ -33,35 +33,32 @@ class Counter {
 
 // Test that Atomic is really working with thread safe
 func testAtomicChange() {
-        // Create an instance of the Test class
-
-        // Define the number of iterations and concurrent threads
-        let iterations = 10_000
-        let numberOfThreads = 4
-
-        // Create a concurrent dispatch queue and a dispatch group
-        let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
-        let dispatchGroup = DispatchGroup()
-
-        // Perform concurrent reads and writes on the shared resource
-        for _ in 0..<iterations {
-            for _ in 0..<numberOfThreads {
-                dispatchGroup.enter()
-                concurrentQueue.async {
-                    self.counter.increment()
-                    dispatchGroup.leave()
-                }
+    // Create an instance of the Test class
+    
+    // Define the number of iterations and concurrent threads
+    let iterations = 10_000
+    let numberOfThreads = 4
+    
+    // Create a concurrent dispatch queue and a dispatch group
+    let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
+    let dispatchGroup = DispatchGroup()
+    
+    // Perform concurrent reads and writes on the shared resource
+    for _ in 0..<iterations {
+        for _ in 0..<numberOfThreads {
+            dispatchGroup.enter()
+            concurrentQueue.async {
+                self.counter.increment()
+                dispatchGroup.leave()
             }
         }
-
-        // Wait for all tasks to finish
-        dispatchGroup.wait()
-
-        // Validate the result
-        let expectedValue = iterations * numberOfThreads
-        assert(counter.value == expectedValue, "Expected value: \(expectedValue), Actual value: \(counter.value)")
-        print("Test passed. Expected value: \(expectedValue), Actual value: \(counter.value)")
-
-}
-
     }
+    
+    // Wait for all tasks to finish
+    dispatchGroup.wait()
+    
+    // Validate the result
+    let expectedValue = iterations * numberOfThreads
+    assert(counter.value == expectedValue, "Expected value: \(expectedValue), Actual value: \(counter.value)")
+    print("Test passed. Expected value: \(expectedValue), Actual value: \(counter.value)")
+}
